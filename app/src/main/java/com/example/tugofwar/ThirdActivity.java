ff3c302bfa86c9d38d8555d6da4b34b750d22e6f
elleizar <43099348+elleizar@users.noreply.github.com>
2020-01-24 21:55:54 -0500

    working changes

--- app/src/main/java/com/example/tugofwar/ThirdActivity.java
+++ app/src/main/java/com/example/tugofwar/ThirdActivity.java
@@ -8,43 +8,42 @@ import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    Button bp1 = (Button) findViewById(R.id.bp1);
-    Button bp2 = (Button) findViewById(R.id.bp2);
+    Button bp1;
+    Button bp2;
     String st1;
     String st2;
 
-    Button start = (Button) findViewById(R.id.s_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        bp1 = (Button) findViewById(R.id.bp1);
+        bp2 = (Button) findViewById(R.id.bp2);
+
+        bp1.setEnabled(false);
+        bp2.setEnabled(false);
+
         st1 = getIntent().getExtras().getString("pl1");
         st2 = getIntent().getExtras().getString("pl2");
-
-        bp1.setOnClickListener(null);
-        bp2.setOnClickListener(null);
 
         bp1.setText(st1);
         bp2.setText(st2);
 
-//        start.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                bp1.setOnClickListener(new View.OnClickListener() {
-//                    public void onClick(View v) {
-//                    }
-//                });
-//                bp2.setOnClickListener(new View.OnClickListener() {
-//                    public void onClick(View v) {
-//                    }
-//                });
-//                start.setVisibility(v.GONE);
-//                String s = "3";
-//
-//
-//            }
-//        });
+        final Button s_button = (Button) findViewById(R.id.s_button);
+        s_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bp1.setEnabled(true);
+                bp2.setEnabled(true);
+                s_button.setVisibility(View.INVISIBLE);
+            }
+        });
+
+//        public void onClick()
+
+
     }
 
 }
